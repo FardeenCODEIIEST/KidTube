108ef65e8150eda23505a21f0743756787270a2e
Fardeen <fardeen786iiestcst@gmail.com>
2025-05-20 00:50:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ function App() {
             title={currentVideo.title}
             width="100%"
             height="315"
-            src={`https://www.youtube.com/embed/${currentVideo.id}?rel=0&controls=0`}
+            src={`https://www.youtube.com/embed/${currentVideo.id}&rel=0&controls=0&modestbranding=1&showinfo=0`}
             allow="autoplay; encrypted-media"
             allowFullScreen
           ></iframe>
