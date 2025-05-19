dae1fa062a5f998d8ace8bee788660d92ffd04b4
Fardeen <fardeen786iiestcst@gmail.com>
2025-05-20 01:20:41 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const API_KEY = "AIzaSyCr3X8StC8CGxNrwho_nFdq1lzMzywkjS0";
 export const CHANNEL_IDS = [
-  "UCzpLlRy6LP4RlVnWSfN_6EQ",
-  "UC4saNBrHNBWOaCXWOOSkRew",
   "UCYqksDldZTkKmsrBb4OGJ9g",
+  "UCqb89GqploCXgz4lsdR-_ww",
+  "UCBnZ16ahKA2DZ_T5W0FPUXg",
+  "UCzpLlRy6LP4RlVnWSfN_6EQ",
 ];
